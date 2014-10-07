1dc1ab8a0640b153842cd4a80c0cd39daf4ff07d
stephanie pi <stephmpi@gmail.com>
2014-10-07 16:16:27 -0700

    add vendor input form

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,4 +1,23 @@
 class VendorsController < ApplicationController
+
   def index
+    @vendors = Vendor.all
   end
+
+  def new
+    @vendor = Vendor.new
+  end
+
+  def create
+    @vendor = Vendor.new(params.require(:vendor).permit(:name, :market_id, :location))
+    if @vendor.save
+      redirect_to root_path
+    else
+
+    end
+
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get "/", to: "vendors#index"
+  get "/vendors",     to: "vendors#index"
+  get "/vendors/new", to: "vendors#new"
+  post "/vendors",    to: "vendors#create"
+  root "vendors#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
