5227a7265b961f3722ddfe4f69b84f51d3d18716
Allie Sterling <sterlina89@gmail.com>
2014-10-10 12:29:53 -0700

    testing out session

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,11 +1,20 @@
 class VendorsController < ApplicationController
 
   def index
+    # session[:blah] = "weird"
+    # session[:id] = "another"
+    # raise session.inspect
     @vendors = Vendor.all
   end
 
   def old
+    # @vendor = Vendor.find(1)
+    # session[:id] = @vendor.id.to_i
+    # session[:name] = @vendor.name
+    # raise session.inspect
     @vendor = Vendor.find_by(name: params[:name])
+    # session[:id] = @vendor.id.to_i
+    # raise params.inspect
     if @vendor == nil
       redirect_to "/vendors/new"
     end
@@ -18,7 +27,11 @@ class VendorsController < ApplicationController
 
   def create
     @vendor = Vendor.new(post_params)
+    #session[:id] = @vendor.id
     if @vendor.save
+      session[:id] = @vendor.id
+      session[:name] = @vendor.name
+      raise session.inspect
       redirect_to root_path
     else
       render :new
